ca9f1d6fcbac6ceca40a249e8b2677863d8789b1
bgordon8 <gordonbrian16@gmail.com>
2020-10-20 20:06:56 -0600

    Adds prettier config for code quality

--- app/api/__tests__/test.js
+++ app/api/__tests__/test.js
@@ -4,13 +4,13 @@ import app from '../src/app'
 const request = supertest(app)
 
 describe('routes : main', () => {
-    describe('GET /', () => {
-        it('returns hello world message', async () => {
-            const res = await request.get("/")
+  describe('GET /', () => {
+    it('returns hello world message', async () => {
+      const res = await request.get('/')
 
-            expect(res.status).toBe(200)
-            expect(res.body).toHaveProperty('message')
-            expect(res.body.message).toBe('hello world!!!! thats whats up!!!')
-        })
+      expect(res.status).toBe(200)
+      expect(res.body).toHaveProperty('message')
+      expect(res.body.message).toBe('hello world!!!! thats whats up!!!')
     })
+  })
 })
--- app/api/src/app.js
+++ app/api/src/app.js
@@ -2,16 +2,16 @@ import express from 'express'
 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "hello world!!!! thats whats up!!!"
-    })
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'hello world!!!! thats whats up!!!',
+  })
 })
 
-if (process.env.NODE_ENV !== "test") {
-    app.listen(3000, () => {
-        console.log('listening on port 3000...')
-    })
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(3000, () => {
+    console.log('listening on port 3000...')
+  })
 }
 
 export default app
