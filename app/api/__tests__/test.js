f8ca1832ede003040e8c453e05621eeaa3f2c798
bgordon8 <gordonbrian16@gmail.com>
2020-10-20 19:51:56 -0600

    Adds intial test skeleton

--- app/api/__tests__/test.js
+++ app/api/__tests__/test.js
@@ -1,5 +1,16 @@
-describe('testing works', () => {
-    it('returns true', () => {
-        expect(true).toBe(true)
+import supertest from 'supertest'
+import app from '../src/app'
+
+const request = supertest(app)
+
+describe('routes : main', () => {
+    describe('GET /', () => {
+        it('returns hello world message', async () => {
+            const res = await request.get("/")
+
+            expect(res.status).toBe(200)
+            expect(res.body).toHaveProperty('message')
+            expect(res.body.message).toBe('hello world!!!! thats whats up!!!')
+        })
     })
 })
